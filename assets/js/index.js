9984d5f204e38cdea6a60fd558c34da1653aaad1
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-04-02 08:37:50 +0200

    Add color change instead

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,15 +12,18 @@ inputs.forEach((el) => {
     if (tar === 3 && valid) {
       resetBtn.disabled = false;
       submitBtn.disabled = false;
-      arrows[tar].style.display = 'inline-block';
-      arrows.forEach((a) => {
-        a.style.backgroundImage =
-          'linear-gradient(to bottom right, transparent 50%, green 0), linear-gradient(to top right, green 50%, transparent 0)';
-      });
+      arrows[tar].classList.add('green');
+
+      // arrows[tar].style.display = 'inline-block';
+      // arrows.forEach((a) => {
+      //   a.style.backgroundImage =
+      //     'linear-gradient(to bottom right, transparent 50%, green 0), linear-gradient(to top right, green 50%, transparent 0)';
+      // });
     }
     if (valid && tar + 1 <= inputs.length) {
       inputs[tar + 1].style.display = 'block';
-      arrows[tar].style.display = 'inline-block';
+      arrows[tar].classList.add('green');
+      console.log(arrows[tar].classList);
     } else {
       inputs[tar + 1].style.display = 'none';
     }
@@ -31,15 +34,12 @@ resetBtn.addEventListener('click', () => {
   form.reset();
 
   for (let i = 1; i < inputs.length; i++) {
-    console.log('got here!');
     inputs[i].value = '';
     inputs[i].style.display = 'none';
   }
 
   for (let i = 0; i < arrows.length; i++) {
-    arrows[i].style.display = 'none';
-    arrows[i].style.backgroundImage =
-      'linear-gradient(to bottom right, transparent 50%, red 0), linear-gradient(to top right, red 50%, transparent 0)';
+    arrows[i].classList.remove('green');
   }
 
   resetBtn.disabled = true;
